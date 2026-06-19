744b321cfd5f9685999a8ec2dd918b0217e6bb7e
Alejandro <alejandro@Alejandros-MacBook-Air.local>
2026-06-19 17:11:38 +0200

    Fix PWA icons for iOS and Android

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,10 @@ export const metadata: Metadata = {
     statusBarStyle: "default",
     title: "Mensa Mate",
   },
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-icon.png",
+  },
   formatDetection: {
     telephone: false,
   },
